09e903377810c867e4296c745ec77a22c904a960
Koninti Nikhitha <nikhithakoninti@gmail.com>
2025-08-26 19:11:53 +0530

    Update SampleName.java

---
+++ SampleName.java
@@ -0,0 +1,9 @@
+public class SampleName {
+    public static void main(String[] args) {
+        // Store name in a variable
+        String name = "Nikhitha";
+        
+        // Print the name
+        System.out.println("My name is " + name);
+    }
+}
